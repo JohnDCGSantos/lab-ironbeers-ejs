d4bab0258680237309b1a4f89c4dfd5d2022ad08
JohnDCGSantos <jdcg.santos@gmail.com>
2023-07-05 00:05:26 +0100

    done

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
 app.use(expressLayouts);
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,6 +18,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi =>{
+    console.log("Beers from dataBase", beersFromApi)
+   res.render("beers", {beersFromApi:beersFromApi})
+  })
+  .catch(error => console.log(error));
+})
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom().then(beer =>{
+  
+    res.render("random-beer", { beer: beer[0]})
+    })/*: randomBeerFromApi }))*/
+    //.catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
